f5f49eae8c54871cb3b2806dc9459ceb6ac95181
naumorek <38781843+naumorek@users.noreply.github.com>
2024-10-15 17:17:09 +0300

    Add files via upload

--- module_6_1.py
+++ module_6_1.py
@@ -75,25 +75,21 @@ class Animal:
         self.name=name
 
 
+    def eat(self, food):
+        if food.edible:
+            self.fed = True
+            print(f'{self.name} съело {food.name}')
+        else:
+            self.alive = False
+            print(f'{self.name} не стал есть  {food.name}')
+
 
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            print(f'{self.name} съело {food.name}')
-        else:
-            self.alive = False
-            print(f'{self.name} не стал есть  {food.name}')
+    pass
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            print(f'{self.name} съело {food.name}')
-        else:
-            self.alive = False
-            print(f'{self.name} не стал есть  {food.name}')
+    pass
 
 class Plant:
     edible = False
